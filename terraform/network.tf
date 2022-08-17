3582d65d5aa8420bf7d4f1fd1a07242f89964f47
Johannes Stang <tubenhirn@gmail.com>
2022-08-17 20:30:32 +0200

    fix: update ip_address output

--- terraform/network.tf
+++ terraform/network.tf
@@ -3,6 +3,6 @@ resource "google_compute_address" "ip_address" {
 }
 
 output "ip_address" {
-  value       = google_compute_address.ip_address
+  value       = google_compute_address.ip_address.address
   description = "static ip for the pihole"
 }
